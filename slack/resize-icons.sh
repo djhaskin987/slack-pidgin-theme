45f3803aabf4695490dd2c4d78cd01eba8759eae
Daniel Haskin <daniel.haskin@angel.com>
2023-10-11 11:38:03 -0600

    Make bigger icons, allow all emoji back

--- slack/resize-icons.sh
+++ slack/resize-icons.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-set -e
+set -ex
+
+cat > theme <<FRONTMATTER
+Name=Slack Emoji Theme
+Description=Slack colon codes and unicode characters
+Icon=1f308.png
+Author=Daniel Haskin
+
+[default]
+FRONTMATTER
+
+tempfile=$(mktemp)
+
 for i in $(ls -1A svg)
 do
     lower_i=$(echo "${i}" | tr '[A-Z]' '[a-z]' | sed -e 's|.svg|.png|g')
-    convert "svg/${i}" -resize 32x32 "${lower_i}"
+    convert "svg/${i}" -resize 64x64 "${lower_i}"
+
+
+    hexes=$(echo "${lower_i}" | sed -e 's|\.png||g' -e 's|^|0x|' -e 's|-| 0x|g')
+    format_hexes=$(echo "${lower_i}" | sed -e 's|\.png||g' -e 's|[^-]\{1,\}|\\\\t\\\\U%08x|g' -e 's|-||g' -e 's|^|%%s|' -e 's|$|\\\\n|')
+    printf $(printf "${format_hexes}" ${hexes}) "${lower_i}" >> "${tempfile}"
     error=$?
     if [ "${error}" -ne 0 ]
     then
@@ -12,3 +29,5 @@ do
         exit 1
     fi
 done
+
+cat "${tempfile}" slacks | sort -u >> theme
